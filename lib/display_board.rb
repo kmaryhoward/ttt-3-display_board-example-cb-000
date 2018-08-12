efc99eb8e756714a9157cfe93a40edd71c41f6ae
Kathleen Howard <kmaryhoward@gmail.com>
2018-08-12 21:20:48 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,6 @@
-cell = "   |   |   "
-line = "-----------"
-def display_board (cell, line)
+def display_board
+  cell = "   |   |   " 
+  line = "-----------"
   2.times { puts cell, line }
   puts cell
-end
-puts display_board(cell, line) # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+end # Define a method display_board that prints a 3x3 Tic Tac Toe Board
